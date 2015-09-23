f3497f67d6a8d86483d6fbe61c563778e36d2195
Michael Warkentin <mwarkentin@gmail.com>
2015-09-23 17:16:18 -0400

    Add index route

--- hello.py
+++ hello.py
@@ -3,8 +3,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/hello')
 def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
